570cdf4b16d3f1e38f7bf469377b9a7a11320df6
jsalgua027 <92090934+jsalgua027@users.noreply.github.com>
2025-03-22 18:15:53 +0100

    Reservas Terminado

--- precios-reserva/src/App.js
+++ precios-reserva/src/App.js
@@ -1,39 +1,33 @@
-import logo from './logo.svg';
+import ComponenteMultiple from "./componets/ComponenteMultiple";
 import './App.css';
 import { useRef, useState } from 'react';
 
 function App() {
+  /**
+   * En este ejercicio trabajo de dos maneras diferentes la gestión de envento
+   * 1)con una función fuera del return
+   * 2)dentro del input
+   */
 
-  const[totalReseva,setTotalReserva]=useState(null);
-  const[dias,setDias]=useState(null);
-  const hotel=useRef(null);
-  const coche=useRef(null);
- 
+  const[dias,setDias]=useState(0);
+  const [opcion,setOpcion]=useState("Hotel");
 
-  const precioDiaHotel=25.5;
-  const precioDiaCoche=15;
-
-  const calcular=(event)=>{
-   
-    setDias(event.target.value);
-    const numDias=parseInt(dias,10);
-    setTotalReserva(numDias*precioDiaCoche);
+  const selectorHotelCoche=(e)=>{
+    setOpcion(e.target.value);
 
   }
 
 
   return (
     <div className='container'>
-      <h1>Reservas</h1>
-      <select className='desplegable' value="">
-        <option ref={hotel}>HOTEL</option>
-        <option ref={coche}>COCHE</option>
+     
+     <select className='desplegable' onChange={selectorHotelCoche}>
+        <option value="Hotel">Hotel</option>
+        <option  value="Coche">Coche</option>
       </select>
-
-      <input  type="text" onChange={calcular}  placeholder='Introduce los dias '></input>
-      {totalReseva && (<p>El precio de la reserva es: {totalReseva} $</p>)}
-
-      
+      <input type="text" onChange={e=>setDias(e.target.value)} placeholder="Introduce el número de días"></input>
+      {opcion==="Hotel"&& <ComponenteMultiple opcion={1} dias={dias}/>}
+      {opcion==="Coche"&& <ComponenteMultiple opcion={2} dias={dias}/>}
     </div>
   );
 }
