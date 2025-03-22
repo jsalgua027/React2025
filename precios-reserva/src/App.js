2d207d61f15412e2e00d21c2ec1936bc5a748fcd
jsalgua027 <92090934+jsalgua027@users.noreply.github.com>
2025-03-22 17:42:05 +0100

    reservas

--- precios-reserva/src/App.js
+++ precios-reserva/src/App.js
@@ -1,23 +1,39 @@
 import logo from './logo.svg';
 import './App.css';
+import { useRef, useState } from 'react';
 
 function App() {
+
+  const[totalReseva,setTotalReserva]=useState(null);
+  const[dias,setDias]=useState(null);
+  const hotel=useRef(null);
+  const coche=useRef(null);
+ 
+
+  const precioDiaHotel=25.5;
+  const precioDiaCoche=15;
+
+  const calcular=(event)=>{
+   
+    setDias(event.target.value);
+    const numDias=parseInt(dias,10);
+    setTotalReserva(numDias*precioDiaCoche);
+
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+      <h1>Reservas</h1>
+      <select className='desplegable' value="">
+        <option ref={hotel}>HOTEL</option>
+        <option ref={coche}>COCHE</option>
+      </select>
+
+      <input  type="text" onChange={calcular}  placeholder='Introduce los dias '></input>
+      {totalReseva && (<p>El precio de la reserva es: {totalReseva} $</p>)}
+
+      
     </div>
   );
 }
