824392fc645c149de5c830106d5cb0170dae5300
jsalgua027 <92090934+jsalgua027@users.noreply.github.com>
2025-03-26 18:01:17 +0100

    styledcomponets prosp

--- styledcomponents/src/App.js
+++ styledcomponents/src/App.js
@@ -1,11 +1,15 @@
-import logo from './logo.svg';
+
 import './App.css';
 import { MiBoton } from './components/styled';
+import { MiBotonLargo } from './components/styled2';
 
 function App() {
   return (
     <div className="App">
-  <MiBoton>Entrar</MiBoton>
+  <MiBoton entrar={true}>Entrar</MiBoton>
+  <MiBoton entrar={false}>Consulta</MiBoton>
+
+  <MiBotonLargo>Salir(boton herencia)</MiBotonLargo>
     </div>
   );
 }
---
+++ styledcomponents/src/components/styled2.js
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+import { MiBoton } from "./styled";
+//con el styled(MiBoton)``  le decimos que hereda de Miboton y despues sobreescibimos lo que queremos modificar
+export const MiBotonLargo=styled(MiBoton)`
+
+width: 300px;
+background-color: red;
+
+
+`
